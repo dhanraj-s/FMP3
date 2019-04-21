8226f7ab036aeef51e294acf51aeac6e7f22abf2
dhanraj-s <dhanrajshrivastav28@gmail.com>
2019-04-21 15:17:13 +0530

    Now uses musicPlayer class

--- main.cpp
+++ main.cpp
@@ -4,66 +4,46 @@
 #include <string>
 
 #include "fmodException.h"
-#include "System.h"
-#include "Sound.h"
-#include "Channel.h"
+#include "musicPlayer.h"
 
 /* This is only to test everything out */
 
 int main()
 {
     try{
-        System sys;
-        Sound music;
-        Channel ch;
-        sys.loadMusic("music.mp3", music);
-        std::cout << "TEST\n";
+        musicPlayer p("music.mp3");
         int choice {0};
-        bool paused {false};
-        bool playing {false};
-        while(choice != 6){
-            float volume = ch.getVolume();
-            if(volume == Channel::INVALID_VOLUME)
+        while(choice != 6) {
+            float volume = p.getVolume();
+            if(volume == musicPlayer::INVALID_VOLUME)
                 std::cout << "VOL : Channel not playing\n";
             else
-                std::cout << "VOL : " << volume * 100 << "\n";
+                std::cout << "VOL : " << volume << "\n";
 
             std::cout << "1.Play\n2.Stop\n3.Pause\n4.Resume\n5.Change Volume\n6.Quit\n";
             std::cout << "Enter your choice: ";
             std::cin >> choice;
 
-            switch(choice){
+            switch(choice) {
                 case 1:
-                    sys.playMusic(music, ch);
-                    playing = true;
-                    paused = (paused)? !paused : paused; //keep playing and paused in sync
+                    p.play();
                     break;
                 case 2:
-                    ch.stopChannel();
-                    playing = false;
-                    paused = (paused)? !paused : paused; //keep playing and paused in sync
+                    p.stop();
                     break;
                 case 3:
-                    if(paused == false && playing == true){
-                        ch.switchPause();
-                        paused = true;
-                    }
+                    p.pause();
                     break;
                 case 4:
-                    if(paused == true && playing == true){
-                        ch.switchPause();
-                        paused = false;
-                    }
+                    p.resume();
                     break;
                 case 5:
                 {
                     float v{0};
                     std::cout << "Enter volume: ";
                     std::cin >> v;
-                    ch.setVolume(v/100);
+                    p.setVolume(v);
                 }
-                default:
-                    break;
             }
         }
     }
